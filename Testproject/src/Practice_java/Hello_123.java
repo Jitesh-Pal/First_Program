acf31812c093be3f02f6d2ea201d2958358d965b
Jitesh-Pal <60747924+Jitesh-Pal@users.noreply.github.com>
2020-02-07 13:26:45 +0530

    First commit

---
+++ Testproject/src/Practice_java/Hello_123.java
@@ -0,0 +1,12 @@
+package Practice_java;
+
+public class Hello_123 {
+
+	public static void main(String[] args) 
+	{
+System.out.println("Hello world");
+
+
+	}
+
+}
---
+++ Testproject/src/module-info.java
@@ -0,0 +1,48 @@
+module Testproject1 {
+}
+
+//a[@class='css-single-active']//span[contains(text(),'9')]
+
+
+		//a[@class='css-single-active']//span[contains(text(),'6')]
+		
+	  //*[@id="guests"]/div[2]/div[2]/div[3]/div[2]
+
+
+//driver.findElement(By.xpath("//*[@id=\"autocomplete-js\"]")).sendKeys("Dublin");
+		//Thread.sleep(5000);
+	    //driver.findElement(By.xpath("//a[@class='css-single-active']//span[contains(text(),'6')]")).click();
+		
+	    /*WebElement db = driver.findElement(By.xpath("//div[@class='icon-left']//i//i[@class='icon icon_check-in']"));
+	    Actions a = new Actions(driver);
+	    a.moveToElement(db).doubleClick().build().perform();
+	    //Thread.sleep(5000);
+	    WebElement da = driver.findElement(By.cssSelector("div.datepicker:nth-child(4) div.datepickerContainer:nth-child(9) tbody:nth-child(1) tr:nth-child(1) td:nth-child(3) table.datepickerViewDays > thead:nth-child(1)"));
+	    a.moveToElement(da).click().build().perform();*/
+
+
+//*[@id="autocomplete-js"]
+
+//div[@class='icon-left user-location-js']
+
+
+//*[@id="withinRange-hotels-js"]/div[9]
+
+//*[@id=\"withinRange-hotels-js\"]/div[3]/div/figcaption
+
+//*[@id="index-page"]/body/view/view/view[2]/div[2]/view/div[1]/div[1]/div[3]/div/ul/li[3]/span
+
+//*[@id=\"flights-page\"]/body/view/view/view[2]/div[2]/view/div[1]/div[1]/div[3]/div/ul/li[3]/span/span
+
+
+//Wait for 3000 second
+//Thread.sleep(1000);
+
+//Book the Hotel
+// driver.findElement(By.xpath("//*[@id=\"index-page\"]/body/view/view/view[2]/div[2]/view/div[1]/div[2]/div[1]/div/div[3]/div/div[2]/div[1]/div[1]/div[2]/div[2]")).click();
+
+//*[@id="withinRange-hotels-js"]/div[4]/div/figcaption
+
+//*[@id="withinRange-hotels-js"]/div[4]/div/figcaption
+
+//*[@id=\"withinRange-hotels-js\"]/div[4]/div/figcaption
